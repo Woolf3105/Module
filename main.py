678cb52c06bc8bba0a79f41c58736f63635ff706
Woolf3105 <ElectraBris@gmail.com>
2024-10-19 20:47:37 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+name = 'Valentina'
+print('Name', ('Valentina'))
+name = '36'
+print('Age', ('36'))
+name = 'A new era'
+print('A new era', ('35'))
+name = 'He is a student'
+print('He is a student', ('Right'))
